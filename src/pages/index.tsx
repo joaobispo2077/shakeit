386ead0b95efd2deb821e338cb4d20aca35f0aa5
João Bispo <joaobispo2077@gmail.com>
2022-09-29 16:47:44 -0300

    refactor: migrate creat react app to next app

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import { ExperienceBar } from '../components/ExperienceBar'
+
+const Home: NextPage = () => {
+
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  )
+}
+
+export default Home
