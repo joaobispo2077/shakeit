ec50ecb991da1c8bb835ae94a5e4fc58b435240e
João Bispo <joaobispo2077@gmail.com>
2022-09-30 16:53:22 -0300

    refactor: extract experience bar style to a css module

--- src/components/ExperienceBar/index.tsx
+++ src/components/ExperienceBar/index.tsx
@@ -1,12 +1,15 @@
+import styles from "../../styles/components/ExperienceBar.module.css";
+
+
 export function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{width: '50%'}}/>
 
         <span 
-          className="current-experience"
+          className={styles.currentExperience}
           style={{left: '50%'}}
         >512 xp</span>
       </div>
