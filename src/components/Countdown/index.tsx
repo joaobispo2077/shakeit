efafd857f5b0267cf3400ca3c7bd737b3c8dd20d
João Bispo <joaobispo2077@gmail.com>
2022-09-30 18:08:17 -0300

    feat: add countdown timeout

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,22 +1,56 @@
+import { useEffect, useState } from "react";
 import styles from "../../styles/components/Countdown.module.css";
 
 
 export function Countdown() {
+  const [time, setTime] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  useEffect(() => {
+    let countdownTimeout: NodeJS.Timeout | null = null;
+
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000)
+    };
+
+    return () => {
+      if (countdownTimeout) {
+        clearTimeout(countdownTimeout)
+      }
+    };
+  }, [isActive, time]);
+
+  function toggleCountdown() {
+    setIsActive((isActive) => !isActive);
+  };
+
   return (
     <div>
       <div className={styles.countdown}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
-      <button type="button" className={styles.countdownButton}>
-        Iniciar um ciclo
+      <button 
+        type="button" 
+        className={styles.countdownButton}
+        onClick={toggleCountdown}  
+      >
+        {isActive ? 'Abandonar ciclo' : 'Iniciar um ciclo'}
       </button>
     </div>
   )
